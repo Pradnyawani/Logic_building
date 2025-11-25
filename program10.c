7915b2ab6105f69c3d1b870656e086981c05635a
Pradnyawani <pradnyawani7@gmail.com>
2025-11-25 15:06:45 +0530

    Add files via upload

---
+++ program10.c
@@ -0,0 +1,93 @@
+/*
+step1:Understand the problem statement
+step2:write the algorithm
+step3:Decide the programming language
+step4:write the program
+step5:test the program
+*/
+
+/*
+ALOGORITHM
+
+START
+    Accept first number as no1
+    Accept second number as no2
+    perform addirion of no1 & no2
+    If the input is negative then convert it into positive
+    Display the addition on screen
+STOP
+*/
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Requried Header file
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :  AdditionTwoNumber
+//  Description :      It is used tp perform addition
+//  Input   :          Float,Float
+//  Output:            Float
+//  Author:            Pradnya Bhausaheb Wani
+//  Date:              09/10/2025
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float AdditionTwoNumbers(float fNo1,float fNo2)
+{
+    float fSum=0.0f;
+
+    if(fNo1<0.0f)  //updator
+    {
+        fNo1 =-fNo1;
+    }
+
+    
+    if(fNo2<0.0f)   //updator
+    {
+        fNo2 =-fNo2;
+    }
+
+    fSum=fNo1+fNo2;    //Business logic
+    return fSum;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    float fValue1=0.0f,fValue2=0.0f,fRet=0.0f;
+
+    printf("Enter first number:\n");
+    scanf("%f",&fValue1);
+
+    printf("Enter second number:\n");
+    scanf("%f",&fValue2);
+
+    fRet =AdditionTwoNumbers(fValue1,fValue2);   
+
+    printf("Addition is :%f\n",fRet);    
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases Succesfully handled by the application 
+//  Input1 :  10.5        Input2 : 3.2    output : 13.7
+//  Input1 :  -10.5       Input2 : 3.2    output : 13.7
+//  Input1 :  10.5        Input2 : -3.2   output : 13.7  
+//  Input1 :  -10.5       Input2 : -3.2   output : 13.7
+//  Input1 :  10.5        Input2 : 0.0    output : 13.7
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
---
+++ program8.c
@@ -0,0 +1,48 @@
+/*
+step1:Understand the problem statement
+step2:write the algorithm
+step3:Decide the programming language
+step4:write the program
+step5:test the program
+*/
+
+/*
+ALOGORITHM
+
+START
+    Accept first number as no1
+    Accept second number as no2
+    perform addirion of no1 & no2
+    Display the addition on screen
+STOP
+*/
+
+
+
+
+
+#include<stdio.h>
+
+float AdditionTwoNumbers(float fNo1,float fNo2)
+{
+    float fSum=0;
+    fSum=fNo1+fNo2;    //Business logic
+    return fSum;
+}
+
+int main()
+{
+    float fValue1=0.0f,fValue2=0.0f,fRet=0.0f;
+
+    printf("Enter first number:\n");
+    scanf("%f",&fValue1);
+
+    printf("Enter second number:\n");
+    scanf("%f",&fValue2);
+
+    fRet =AdditionTwoNumbers(fValue1,fValue2);   
+
+    printf("Addition is :%f\n",fRet);    
+
+    return 0;
+}
