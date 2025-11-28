da13e1489cad4d69bcdca5e5e9e110a492bd3805
Pradnyawani <pradnyawani7@gmail.com>
2025-11-28 11:31:57 +0530

    Add files via upload

---
+++ program27.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+//Dynamic function 
+
+void Display(int iFrequency)
+{
+    int iCnt=0;
+    if(iFrequency<0)
+    {
+        printf("invalid input");
+        return;
+    }
+    for(iCnt=1;iCnt<=iFrequency;iCnt++)
+    {
+     printf("Jay Ganesh.....\n");
+    }
+}
+
+
+int main()
+{
+    int iCount=0;
+
+    printf("Enter the Frequency:\n");
+    scanf("%d",&iCount);
+
+    Display(iCount);    
+
+    return 0;
+}
