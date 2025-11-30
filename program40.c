c98c1c07fe5416e0bbddc12ceb0c1f1d12ac9e1a
Pradnyawani <pradnyawani7@gmail.com>
2025-11-30 09:34:15 +0530

    Add files via upload

---
+++ program40.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+     void Display(int iNo)
+    {
+        int iCnt=0;
+
+
+
+        for(iCnt=2;iCnt<=iNo;iCnt=iCnt+2)
+        {
+            printf("%d\t",iCnt);
+        }
+
+    printf("\n");
+    }
+int main()
+{
+    int iValue=0;
+
+    printf("please inter frequency: \n");
+    scanf("%d",&iValue);
+    Display(iValue);
+    return 0;
+}
